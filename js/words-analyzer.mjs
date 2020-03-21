e242103d93d11b9e38a51e77a0213e4ffe7529e8
SheepTester <ussa.say@gmail.com>
2020-03-20 20:47:53 -0700

    Use most used case form

--- js/words-analyzer.mjs
+++ js/words-analyzer.mjs
@@ -31,3 +31,38 @@ export function digest (text) {
         .filter(word => word))
     .filter(sentence => sentence.length)
 }
+
+
+/**
+ * "Chyme or chymus is the semi-fluid mass of partly digested food that is
+ * expelled by the stomach"
+ *   - Wikipedia https://en.wikipedia.org/wiki/Chyme
+ *
+ * Makes the cases of each word the same based on which is used most often.
+ * This is to allow weird words like "I" and "Albert" to be capitalized
+ */
+export function preferredCase (chyme) {
+  const formSet = new Map()
+  for (const word of [].concat(...chyme)) {
+    const lowercase = word.toLowerCase()
+    if (!formSet.has(lowercase)) formSet.set(lowercase, new Map())
+
+    const forms = formSet.get(lowercase)
+    forms.set(word, (forms.get(word) || 0) + 1)
+  }
+  for (const [lowercase, forms] of formSet) {
+    let max = 0
+    let maxForm = lowercase
+    for (const [form, frequency] of forms) {
+      if (frequency > max) {
+        max = frequency
+        maxForm = form
+      }
+    }
+    formSet.set(lowercase, maxForm)
+  }
+  return chyme
+    .map(sentence =>
+      sentence
+        .map(word => formSet.get(word.toLowerCase())))
+}
