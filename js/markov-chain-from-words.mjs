87065cc13341b2098d0da906aac7f5245e4302d3
SheepTester <ussa.say@gmail.com>
2020-03-22 15:16:47 -0700

    convert weird markov chain thing to a class

--- js/markov-chain-from-words.mjs
+++ js/markov-chain-from-words.mjs
@@ -3,74 +3,83 @@ import { digest, preferredCase } from './words-analyzer.mjs'
 
 const bound = '[bound]'
 
-export function markovChainFromWords (text, minUsage = 0) {
-  const sentences = preferredCase(digest(text))
-  const wordTypes = new Map()
-  wordTypes.set(bound, Infinity)
-  for (const sentence of sentences) {
-    for (const word of sentence) {
-      wordTypes.set(word, (wordTypes.get(word) || 0) + 1)
-    }
+export class WordFrequencies {
+  constructor (matrix, words) {
+    this.matrix = matrix
+    this.words = words
   }
-  const words = [...wordTypes.keys()]
-    .filter(word => wordTypes.get(word) >= minUsage)
-  const matrix = new Matrix(words.length, words.length)
-  const boundIndex = words.indexOf(bound)
-  for (const sentence of sentences) {
-    let lastIndex = boundIndex
-    for (const word of sentence) {
-      const index = words.indexOf(word)
-      if (index === -1) {
-        // Word is not used enough, so skip
-        continue
+
+  markovChain () {
+    const chain = this.matrix.clone()
+    for (let row = 0; row < chain.rows; row++) {
+      let sum = 0
+      for (let col = 0; col < chain.cols; col++) {
+        sum += chain.get(row, col)
       }
-      matrix.set(lastIndex, index, matrix.get(lastIndex, index) + 1)
-      lastIndex = index
-    }
-    matrix.set(lastIndex, boundIndex, matrix.get(lastIndex, boundIndex) + 1)
-  }
-  return {
-    matrix,
-    words
-  }
-}
-
-export function normalize (chain) {
-  for (let row = 0; row < chain.rows; row++) {
-    let sum = 0
-    for (let col = 0; col < chain.cols; col++) {
-      sum += chain.get(row, col)
-    }
-    for (let col = 0; col < chain.cols; col++) {
-      chain.set(row, col, chain.get(row, col) / sum)
-    }
-  }
-  return chain
-}
-
-export function combine (...sets) {
-  const wordUnion = [...new Set([].concat(...sets.map(({ words }) => words)))]
-  const bigger = new Matrix(wordUnion.length, wordUnion.length)
-  const addMatrixDataFor = ({ matrix, words }) => {
-    for (let row = 0; row < matrix.rows; row++) {
-      for (let col = 0; col < matrix.cols; col++) {
-        const position = wordUnion.indexOf(words[row]) * bigger.cols + wordUnion.indexOf(words[col])
-        bigger.data[position] += matrix.data[row * matrix.cols + col]
+      for (let col = 0; col < chain.cols; col++) {
+        chain.set(row, col, chain.get(row, col) / sum)
       }
     }
+    return chain
   }
-  sets.forEach(addMatrixDataFor)
-  return { matrix: bigger, words: wordUnion }
-}
 
-export function toFile ({ matrix, words }) {
-  return `${words.join('\n')}\n=\n${matrix.toFile()}`
-}
+  combineWith (...sets) {
+    return WordFrequencies.combine(this, ...sets)
+  }
 
-export function fromFile (file) {
-  const [words, matrix] = file.split('=')
-  return {
-    matrix: Matrix.fromFile(matrix),
-    words: words.split(/\r?\n/).filter(word => word)
+  toFile ({ matrix, words }) {
+    return `${words.join('\n')}\n=\n${matrix.toFile()}`
+  }
+
+  static fromFile (file) {
+    const [words, matrix] = file.split('=')
+    return new WordFrequencies(
+      Matrix.fromFile(matrix),
+      words.split(/\r?\n/).filter(word => word)
+    )
+  }
+
+  static fromWords (text, minUsage = 0) {
+    const sentences = preferredCase(digest(text))
+    const wordTypes = new Map()
+    wordTypes.set(bound, Infinity)
+    for (const sentence of sentences) {
+      for (const word of sentence) {
+        wordTypes.set(word, (wordTypes.get(word) || 0) + 1)
+      }
+    }
+    const words = [...wordTypes.keys()]
+      .filter(word => wordTypes.get(word) >= minUsage)
+    const matrix = new Matrix(words.length, words.length)
+    const boundIndex = words.indexOf(bound)
+    for (const sentence of sentences) {
+      let lastIndex = boundIndex
+      for (const word of sentence) {
+        const index = words.indexOf(word)
+        if (index === -1) {
+          // Word is not used enough, so skip
+          continue
+        }
+        matrix.set(lastIndex, index, matrix.get(lastIndex, index) + 1)
+        lastIndex = index
+      }
+      matrix.set(lastIndex, boundIndex, matrix.get(lastIndex, boundIndex) + 1)
+    }
+    return new WordFrequencies(matrix, words)
+  }
+
+  static combine (...sets) {
+    const wordUnion = [...new Set([].concat(...sets.map(({ words }) => words)))]
+    const bigger = new Matrix(wordUnion.length, wordUnion.length)
+    const addMatrixDataFor = ({ matrix, words }) => {
+      for (let row = 0; row < matrix.rows; row++) {
+        for (let col = 0; col < matrix.cols; col++) {
+          const position = wordUnion.indexOf(words[row]) * bigger.cols + wordUnion.indexOf(words[col])
+          bigger.data[position] += matrix.data[row * matrix.cols + col]
+        }
+      }
+    }
+    sets.forEach(addMatrixDataFor)
+    return new WordFrequencies(bigger, wordUnion)
   }
 }
