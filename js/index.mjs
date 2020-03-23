648e11e880c5c1b27057fb1d90f7d4d3c94766a7
SheepTester <ussa.say@gmail.com>
2020-03-22 19:15:18 -0700

    Make the list disappear when you move your cursor I suppose, and build

--- js/index.mjs
+++ js/index.mjs
@@ -1,44 +1,49 @@
 import getCaretCoordinates from 'textarea-caret'
 
-const IDEAL_LIST_HEIGHT = 300
+const MAX_LIST_HEIGHT = 300
 const autocompleteList = document.getElementById('autocomplete')
 const input = document.getElementById('input')
-function updateAutocomplete (e) {
-  if (e.type === 'input') {
-    if (input.selectionStart === input.selectionEnd) {
-      autocompleteList.classList.remove('hidden')
-    }
-  }
+function moveAutocomplete () {
   if (!autocompleteList.classList.contains('hidden')) {
-    if (input.selectionStart !== input.selectionEnd) {
-      autocompleteList.classList.add('hidden')
-      return
-    }
     const windowWidth = window.innerWidth
     const windowHeight = window.innerHeight
     const scrollY = input.scrollTop
-    const { width } = autocompleteList.getBoundingClientRect()
+    const { width, height } = autocompleteList.getBoundingClientRect()
     const { top, left, height: cursorHeight } = getCaretCoordinates(input, input.selectionEnd)
     if (left + width > windowWidth) {
       autocompleteList.style.left = windowWidth - width + 'px'
     } else {
       autocompleteList.style.left = left + 'px'
     }
-    if (top - scrollY + cursorHeight + IDEAL_LIST_HEIGHT > windowHeight) {
-      if (top - IDEAL_LIST_HEIGHT < 0) {
+    if (top - scrollY + cursorHeight + MAX_LIST_HEIGHT > windowHeight) {
+      if (top - MAX_LIST_HEIGHT < 0) {
         // There's no room either way, so might as well squish below instead
         autocompleteList.style.top = top - scrollY + cursorHeight + 'px'
-        autocompleteList.style.height = windowHeight - (top + cursorHeight + IDEAL_LIST_HEIGHT) + 'px'
+        autocompleteList.style.maxHeight = windowHeight - (top + cursorHeight + MAX_LIST_HEIGHT) + 'px'
       } else {
-        autocompleteList.style.top = top - scrollY - IDEAL_LIST_HEIGHT + 'px'
-        autocompleteList.style.height = IDEAL_LIST_HEIGHT + 'px'
+        autocompleteList.style.top = top - scrollY - height + 'px'
+        autocompleteList.style.maxHeight = null
       }
     } else {
       autocompleteList.style.top = top - scrollY + cursorHeight + 'px'
-      autocompleteList.style.height = IDEAL_LIST_HEIGHT + 'px'
+      autocompleteList.style.maxHeight = null
     }
   }
 }
-input.addEventListener('input', updateAutocomplete)
-input.addEventListener('scroll', updateAutocomplete)
-document.addEventListener('selectionchange', updateAutocomplete)
+input.addEventListener('input', e => {
+  if (input.selectionStart === input.selectionEnd) {
+    autocompleteList.classList.remove('hidden')
+  } else {
+    autocompleteList.classList.add('hidden')
+  }
+  moveAutocomplete()
+})
+input.addEventListener('scroll', moveAutocomplete)
+document.addEventListener('selectionchange', () => {
+  if (document.activeElement === input) {
+    if (input.selectionStart !== input.selectionEnd) {
+      autocompleteList.classList.add('hidden')
+    }
+    moveAutocomplete()
+  }
+})
