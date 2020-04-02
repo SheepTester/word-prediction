c30b9aa6de11ab5dce89aadd636a78cab183cc18
SheepTester <ussa.say@gmail.com>
2020-04-02 13:43:36 -0700

    Remake markov chain magic maybe

--- js/core/markers.mjs
+++ js/core/markers.mjs
@@ -3,3 +3,5 @@ export const NUMBER = '[number]'
 export const COMMA = ','
 
 export const BOUND = '[bound]'
+
+export const SEP = '/'
---
+++ js/core/word-markov-chain.mjs
@@ -0,0 +1,90 @@
+import { digest, preferredCase } from './words-analyzer.mjs'
+import { BOUND, SEP } from './markers.mjs'
+
+/**
+ * A more efficient Markov chain that doesn't use matrices and supports higher
+ * orders
+ */
+export class WordMarkovChain {
+  constructor (order = 1, data = new Map()) {
+    this.order = order
+    this._data = data
+  }
+
+  // Use spread operator to clone array lol
+  _getTuple (...words) {
+    while (words.length < order) {
+      words.unshift(BOUND)
+    }
+    return words.join(SEP)
+  }
+
+  getFrequencies (words, normalize = true) {
+    const tuple = this._getTuple(words)
+    const freqMap = this._data.get(tuple)
+    if (freqMap) {
+      const frequencies = [...freqMap]
+      if (normalize) {
+        const total = frequencies.reduce((acc, curr) => acc + curr[1], 0)
+        for (const pair of frequencies) {
+          pair[1] /= total
+        }
+      }
+      return frequencies
+    } else {
+      return []
+    }
+  }
+
+  pickRandom (words) {
+    const frequencies = this.getFrequencies(words, false)
+    if (frequencies.length === 0) return null
+    const total = frequencies.reduce((acc, curr) => acc + curr[1], 0)
+    let random = Math.floor(Math.random() * total)
+    for (const [word, frequency] of frequencies) {
+      random -= frequency
+      if (random < 0) return word
+    }
+    return null
+  }
+
+  toFile () {
+    return JSON.stringify({
+      order: this.order,
+      data: this._data
+    }, null, 2)
+  }
+
+  static fromFile (file) {
+    const { order, data } = JSON.parse(file)
+    return new WordMarkovChain(order, data)
+  }
+
+  static fromWords (text, target) {
+    if (typeof target === 'number') {
+      target = new WordMarkovChain(target)
+    } else if (!target) {
+      target = new WordMarkovChain()
+    }
+    const words = [BOUND]
+      .concat(...preferredCase(digest(text))
+        .map(arr => [...arr, BOUND]))
+    // Pad array with BOUND for beginning and end
+    // eg (BOUND, BOUND, "first") -> "second"
+    for (let i = 1; i < order; i++) {
+      words.unshift(BOUND)
+      words.push(BOUND)
+    }
+    for (let i = 0; i < words.length - order; i++) {
+      const tuple = words.slice(i, i + order).join(SEP)
+      const word = words[i + order]
+      let freqMap = target._data.get(tuple)
+      if (!freqMap) {
+        freqMap = new Map()
+        target._data.set(tuple, freqMap)
+      }
+      freqMap.set(word, (freqMap.get(word) || 0) + 1)
+    }
+    return target
+  }
+}
